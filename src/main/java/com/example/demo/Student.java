175dc3e44b2dc2ced7c7c083ee00865709b78b68
SivaVaishnavi <vaishnavipathivada32@gmail.com>
2026-04-10 10:07:11 +0530

    commit

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "student")
